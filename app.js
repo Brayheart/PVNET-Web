8a63860f6943ac1587663638dc701a92279c70ee
ThomasLiARDJAVA <thomasligreat11@gmail.com>
2019-01-20 16:10:24 -0800

    deleted testtable

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var testtable = require('./routes/mysql');
+//var testtable = require('./routes/mysql');
 
 var app = express();
 
