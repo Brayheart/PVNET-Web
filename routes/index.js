2147ae4795afbcd04723462dab584f8c2757898e
ThomasLiARDJAVA <thomasligreat11@gmail.com>
2019-01-20 21:38:41 -0800

    changed instructor

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/', function(req, res, next) {
     //     filter = 'WHERE instructor = ?';
     // }
 
-    db.query("SELECT * FROM SummerClasses WHERE instructor='Thomas';",function(err, rows) {
+    db.query("SELECT * FROM SummerClasses WHERE instructor='Yuichi';",function(err, rows) {
         if (err) {
             console.log(err);
         }
