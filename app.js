a8b7025fef5937cb2f5f9e84ff6c002cc0a97091
ThomasLiARDJAVA <thomasligreat11@gmail.com>
2019-01-20 16:18:39 -0800

    added testtable.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var testtable = require('./public/javascripts/mysql');
+var testtable = require('./routes/testtable');
 
 var app = express();
 
