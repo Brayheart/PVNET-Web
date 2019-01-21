87487c1f84a0beacddfe6937862a5cb33addc1ee
ThomasLiARDJAVA <thomasligreat11@gmail.com>
2019-01-20 17:38:05 -0800

    one more try

--- routes/index.js
+++ routes/index.js
@@ -18,28 +18,28 @@ con.connect(function(err) {
     });
 });
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//     var db = req.con;
-//     var data = "";
-//
-//     var user = "classUser";
-//
-//     var filter = "";
-//     if (user) {
-//         filter = 'WHERE userid = ?';
-//     }
-//
-//     db.query("SELECT * FROM SummerClasses WHERE instructor='Doug';", user, function(err, rows) {
-//         if (err) {
-//             console.log(err);
-//         }
-//         var data = rows;
-//
-//         // use index.ejs
-//         res.render('index',{page_title:"Test Table",data:rows});
-//     });
-//
-// });
+router.get('/', function(req, res, next) {
+    var db = req.con;
+    var data = "";
+
+    var user = "classUser";
+
+    var filter = "";
+    if (user) {
+        filter = 'WHERE userid = ?';
+    }
+
+    db.query("SELECT * FROM SummerClasses WHERE instructor='Doug';", user, function(err, rows) {
+        if (err) {
+            console.log(err);
+        }
+        var data = rows;
+
+        // use index.ejs
+        res.render('index',{page_title:"Test Table",data:rows});
+    });
+
+});
 exports.list = function(req, res){
 
     req.getConnection(function(err,connection){
