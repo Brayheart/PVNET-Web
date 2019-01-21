b633a4fa1be00301f3609812240766712e229ba6
ThomasLiARDJAVA <thomasligreat11@gmail.com>
2019-01-20 17:36:47 -0800

    one more

--- routes/index.js
+++ routes/index.js
@@ -9,29 +9,37 @@ var con = mysql.createConnection({
     database: "TestDatabase"
 });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    var db = req.con;
-    var data = "";
 
-    var user = "classUser";
-
-    var filter = "";
-    if (user) {
-        filter = 'WHERE userid = ?';
-    }
-
-    db.query("SELECT * FROM SummerClasses WHERE instructor='Doug';", user, function(err, rows) {
-        if (err) {
-            console.log(err);
-        }
-        var data = rows;
-
-        // use index.ejs
-        res.render('index',{page_title:"Test Table",data:rows});
+con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM SummerClasses WHERE instructor='Doug';", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
     });
-
 });
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//     var db = req.con;
+//     var data = "";
+//
+//     var user = "classUser";
+//
+//     var filter = "";
+//     if (user) {
+//         filter = 'WHERE userid = ?';
+//     }
+//
+//     db.query("SELECT * FROM SummerClasses WHERE instructor='Doug';", user, function(err, rows) {
+//         if (err) {
+//             console.log(err);
+//         }
+//         var data = rows;
+//
+//         // use index.ejs
+//         res.render('index',{page_title:"Test Table",data:rows});
+//     });
+//
+// });
 exports.list = function(req, res){
 
     req.getConnection(function(err,connection){
