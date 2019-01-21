9f409f3d00caa4b89f04fa911d6ee34138e0290a
ThomasLiARDJAVA <thomasligreat11@gmail.com>
2019-01-20 17:32:18 -0800

    sql try again

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "classUser",
+    password: "P00bapop!",
+    database: "TestDatabase"
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,14 +22,13 @@ router.get('/', function(req, res, next) {
         filter = 'WHERE userid = ?';
     }
 
-    db.query("SELECT * FROM SummerClasses WHERE instructor='Doug';" + filter, user, function(err, rows) {
-        if (err) {
-            console.log(err);
-        }
-        var data = rows;
+    req.getConnection(function(err,connection){
+        var query = connection.query("SELECT * FROM SummerClasses WHERE instructor='Doug';",function(err,rows){
+            if(err)
+                console.log("Error Selecting : %s ",err );
 
-        // use index.ejs
-        res.render('index',{page_title:"Test Table",data:rows});
+            res.render('index',{page_title:"Test Table",data:rows});
+        });
     });
 
 });
