42ec8702fd13c186d69f579ea0142e9cb189c1d1
ThomasLiARDJAVA <thomasligreat11@gmail.com>
2019-01-23 14:30:18 -0800

    fixed edit

--- routes/index.js
+++ routes/index.js
@@ -116,9 +116,24 @@ router.post('/userEdit', function(req, res, next) {
     var id = req.body.id;
 
     var sql = {
-        userid: req.body.userid,
-        password: req.body.password,
-        email: req.body.email
+        Class_ID: req.body.Class_ID,
+        Class_Name: req.body.Class_Name,
+        Department: req.body.Department,
+        Class_Number: req.body.Class_Number,
+        Class_Section: req.body.Class_Section,
+        Activate: req.body.Activate,
+        Instructor: req.body.Instructor,
+        Internal_Notes: req.body.Internal_Notes,
+        Class_Description: req.body.Class_Description,
+        Additional_Info: req.body.Additional_Info,
+        Prerequisite: req.body.Prerequisite,
+        Start_Week: req.body.Start_Week,
+        End_Week: req.body.End_Week,
+        Age_Start: req.body.Age_Start,
+        Age_End: req.body.Age_End,
+        Consummables_Fee: req.body.Consummables_Fee,
+        Hourly_Fee: req.body.Hourly_Fee,
+        Registration_Fee: req.body.Registration_Fee
     };
 
     var qur = db.query('UPDATE SummerClasses SET ? WHERE instructor = ?', [sql, id], function(err, rows) {
