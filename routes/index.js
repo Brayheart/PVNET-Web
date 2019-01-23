6c52f3cd1aac279b8e3ed5851feaa053ce47b518
ThomasLiARDJAVA <thomasligreat11@gmail.com>
2019-01-23 13:20:56 -0800

    changed querybody

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
     var data = "";
 
     var user = "";
-    var user = req.query.user;
+    var user = req.body.user;
 
     var filter = "";
     if (user) {
@@ -27,6 +27,60 @@ router.get('/', function(req, res, next) {
 });
 
 
+// add post
+router.post('/userAdd', function(req, res, next) {
+
+    var db = req.con;
+
+    // check userid exist
+    var userid = req.body.userid;
+    var qur = db.query('SELECT * FROM SummerClasses WHERE instructor = ?', userid, function(err, rows) {
+        if (err) {
+            console.log(err);
+        }
+
+        var count = rows.length;
+        if (count > 0) {
+
+            var msg = 'Userid already exists.';
+            res.render('userAdd', { title: 'Add User', msg: msg });
+
+        } else {
+
+            var sql = {
+                Class_ID: req.body.Class_ID,
+                Class_Name: req.body.Class_Name,
+                Department: req.body.Department
+                Class_Number: req.body.Class_Number,
+                Class_Section: req.body.Class_Section,
+                Activate: req.body.Activate,
+                Instructor: req.body.Instructor,
+                Internal_Notes: req.body.Internal_Notes,
+                Class_Description: req.body.Class_Description,
+                Additional_Info: req.body.Additional_Info,
+                Prerequisite: req.body.Prerequisite,
+                Start_Week: req.body.Start_Week,
+                End_Week: req.body.End_Week,
+                Age_Start: req.body.Age_Start,
+                Age_End: req.body.Age_End,
+                Consummables_Fee: req.body.Consummables_Fee,
+                Hourly_Fee: req.body.Hourly_Fee,
+                Registration_Fee: req.body.Registration_Fee
+            };
+
+            //console.log(sql);
+            var qur = db.query('INSERT INTO account SET ?', sql, function(err, rows) {
+                if (err) {
+                    console.log(err);
+                }
+                res.setHeader('Content-Type', 'application/json');
+                res.redirect('/');
+            });
+        }
+    });
+
+
+});
 
 
  module.exports = router;
