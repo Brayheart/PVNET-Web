3607c53423761b30d2c9b8b2a1bc7f39f2708ea0
ThomasLiARDJAVA <thomasligreat11@gmail.com>
2019-01-27 16:17:34 -0800

    changed urlencoded to true

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,7 @@ router.get('/', function(req, res, next) {
 
 });
 
-router.post('/SearchInstructor', function(req, res, next) {
+router.post('/InstructorSearch', function(req, res, next) {
 
     var db = req.con;
 
