a8e49505a9b2155758577813048cd2d038da37a6
ThomasLiARDJAVA <thomasligreat11@gmail.com>
2019-01-27 16:23:48 -0800

    debug

--- routes/index.js
+++ routes/index.js
@@ -40,26 +40,26 @@ router.post('/InstructorSearch', function(req, res, next) {
         res.redirect('/InstructorSearch');
     });
 });
-router.get('/InstructorSearch', function(req, res, next) {
-
-    var db = req.con;
-    var data = "";
-
-    var user = req.body.Instructor;
-    console.log("this is get request");
-
-
-    db.query("SELECT * FROM  SummerClasses WHERE Instructor = 'Doug';", user, function(err, rows) {
-        if (err) {
-            console.log(err);
-        }
-        var data = rows;
-        // use index.ejs
-        res.render('SearchIns', { title: 'Test Table', data: data, user: user });
-
-    });
-
-});
+// router.get('/InstructorSearch', function(req, res, next) {
+//
+//     var db = req.con;
+//     var data = "";
+//
+//     var user = req.body.Instructor;
+//     console.log("this is get request");
+//
+//
+//     db.query("SELECT * FROM  SummerClasses WHERE Instructor = 'Doug';", user, function(err, rows) {
+//         if (err) {
+//             console.log(err);
+//         }
+//         var data = rows;
+//         // use index.ejs
+//         res.render('SearchIns', { title: 'Test Table', data: data, user: user });
+//
+//     });
+//
+// });
 
 router.get('/add', function(req, res, next) {
 
